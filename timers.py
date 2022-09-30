e1cb20dbefaf4e77bcb7b4d73c541c968318bfc6
Naeemur Rahman <Naeemur.m.rahman@utu.fi>
2022-10-01 01:34:30 +0300

    Initial State Machine Skeleton
    Adding basic state machine functionality.
    State Machine and It's Mandatory Events

---
+++ timers.py
@@ -0,0 +1,52 @@
+import asyncio
+
+
+async def decrease_connect_retry_timer(self):
+    """Decrease connect_retry_timer every second if its value is greater than zero"""
+
+    self.logger.debug("Starting decrease_connect_retry_timer() coroutine")
+
+    if not hasattr(self, "connect_retry_timer"):
+        self.connect_retry_timer = 0
+
+    while True:
+        await asyncio.sleep(1)
+        if self.connect_retry_timer:
+            self.logger.debug(f"connect_retry_timer = {self.connect_retry_timer}")
+            self.connect_retry_timer -= 1
+            if not self.connect_retry_timer:
+                self.enqueue_event("ConnectRetryTimer_Expires")
+
+
+async def decrease_hold_timer(self):
+    """Decrease hold_timer every second if its value is greater than zero"""
+
+    self.logger.debug("Starting decrease_hold_timer() coroutine")
+
+    if not hasattr(self, "hold_timer"):
+        self.hold_timer = 0
+
+    while True:
+        await asyncio.sleep(1)
+        if self.hold_timer:
+            self.logger.debug(f"hold_timer = {self.hold_timer}")
+            self.hold_timer -= 1
+            if not self.hold_timer:
+                self.enqueue_event("HoldTimer_Expires")
+
+
+async def decrease_keepalive_timer(self):
+    """Decrease keepalive_timer every second if its value is greater than zero"""
+
+    self.logger.debug("Starting decrease_keepalive_timer() coroutine")
+
+    if not hasattr(self, "keepalive_timer"):
+        self.keepalive_timer = 0
+
+    while True:
+        await asyncio.sleep(1)
+        if self.keepalive_timer:
+            self.logger.debug(f"keepalive_timer = {self.keepalive_timer}")
+            self.keepalive_timer -= 1
+            if not self.keepalive_timer:
+                self.enqueue_event("KeepaliveTimer_Expires")
