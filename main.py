fe4f21637317821b90b9056fdaeaa1bc83595f6b
E. Zorman <katalenca@gmail.com>
2022-10-01 18:25:36 +0300

    Adding more route generation logic

--- main.py
+++ main.py
@@ -11,15 +11,20 @@ will be handled by the following steps:
 understandable to the user
 """
 import argparse
-from collections import defaultdict
-from pprint import pprint
 
-from simulation import start_simulation
+from simulation import (
+    generate_routing_paths,
+    setup_as_routers,
+    start_simulation,
+)
 
 
 def parse_args():
+    """
+    Parse arguments for the main program.
+    """
     parser = argparse.ArgumentParser(
-        description="A scalable and customizable simulator of inter-AS routing using BGP "
+        description="A scalable and customizable simulator of AS routing using BGP-4 "
         "protocol."
     )
     parser.add_argument(
@@ -31,130 +36,26 @@ def parse_args():
     parser.add_argument(
         "--router-number",
         type=int,
-        help="The number of routers within each AS to be used in the simulation.",
+        help="The number of routers within the scope of the simulation.",
         default=10,
     )
     return parser.parse_args()
 
 
-def print_path_table(net_dict):
-    for key, item in list(net_dict["paths"].items()):
-        paths_str = f"{key}:\t{item}".expandtabs(8)
-        print(paths_str)
-
-    print()
-    return
-
-
-def setup_router_paths(as_num, router_num):
-    print(
-        "By default, each AS will be connected to other random AS's.Hence the network \n"
-        "topology will be different each simulation, unless configured otherwise.\n"
-    )
-
-    network_dict = {
-        "AS_topology": defaultdict(list),
-        "paths": {},
-    }
-
-    # distribute the routers to their corresponding AS, based on simple modulo arithmetics
-    for i in range(router_num):
-        as_location = i % as_num
-        network_dict["AS_topology"][f"A{as_location+1}"].append(f"R{i+1}")
-
-        # create empty path sets for each router
-        network_dict["paths"][f"R{i+1}"] = []
-
-    custom_paths = input(
-        "Do you wish to set any specific routes for any of the routers that will be created? Y/N\n"
-    )
-    if custom_paths.upper() == "N":
-        return network_dict
-
-    if custom_paths.upper() != "Y":
-        print("Wrong answer :O")
-        return None
-
-    loop = True
-    while loop:
-        # Get the router to customise its path
-        router_of_choice = input(
-            f"Which router's path would you like to customise?\n"
-            f"Possible choices: {list(network_dict['paths'])}\n"
-            "You can exit the customisation at any point by selecting Q.\n"
-        ).upper()
-        # exit or drop incorrect input
-        if router_of_choice == "Q":
-            loop = False
-            continue
-
-        elif router_of_choice not in network_dict["paths"]:
-            print("Incorrect value chosen. Aborting customization...\n")
-            continue
-
-        choice_path = input(
-            f"To which routers would you like your router to have a path?\n"
-            f"Possible choices: {list(network_dict['paths'])}\n"
-            "You can also set a list of routes you wish to add, e.g. <r1 r2 r4>.\n"
-            "You can exit the customisation at any point by selecting Q.\n"
-        ).upper()
-
-        paths = choice_path.split(" ")
-
-        if "Q" in paths:
-            loop = False
-            continue
-
-        # get the AS of the chosen router
-        router_as = [
-            i
-            for i in network_dict["AS_topology"]
-            if router_of_choice in network_dict["AS_topology"][i]
-        ]
-
-        # if any element in paths is also in the AS of the chosen router, we have some
-        # overlapping in paths that we do not want, skip the choice made
-        if bool(set(paths) & set(network_dict["AS_topology"][router_as[0]])):
-            print("Incorrect value chosen. Aborting customization...\n")
-            continue
-
-        tmp_paths = set(network_dict["paths"][router_of_choice])
-
-        if set(paths).issubset(tmp_paths):
-            print("Path(s) already contained. Aborting customization...\n")
-            continue
-
-        tmp_paths.update(set(paths))
-        network_dict["paths"][router_of_choice] = list(tmp_paths)
-
-        print_path_table(network_dict)
-
-    return network_dict
-
-
-def generate_routing_paths(as_number, router_number, custom_paths=None):
-    """
-    Do we want to make all the routers be able to talk to each router in the AS
-    and only have custom paths if the user says so, or do we randomise the paths
-    originally and only give specific paths if the user wants them... Hmmm
-    """
-    pass
-
-
 def main():
+    """
+    Entrypoint for the simulation program.
+    """
     args = parse_args()
     print(
         "Welcome to our scalable and customizable simulator of AS routing using "
         "the BGP-4 protocol.\nThe current network topology is the following: \n"
         f"\t Number of AS's: {args.as_number}\n"
-        f"\t Number of routers: {args.router_number}"
+        f"\t Number of routers: {args.router_number}".expandtabs(2)
     )
 
-    custom_paths = setup_router_paths(args.as_number, args.router_number)
-    if not custom_paths:
-        generate_routing_paths(args.as_number, args.router_number)
-    else:
-        generate_routing_paths(args.as_number, args.router_number, custom_paths)
+    network_data = setup_as_routers(args.as_number, args.router_number)
+    generate_routing_paths(args.as_number, network_data)
 
     start_simulation()
 
--- simulation.py
+++ simulation.py
@@ -2,7 +2,179 @@
 Handle the processing of the simulations after the environment
 has been set up.
 """
+import random
+from collections import defaultdict
+from pprint import pprint
+
+
+def generate_routing_paths(as_num, network_data):
+    """
+    Randomly assign paths from one AS to another, expect if the user specified
+    some routes. Then we respect those choices and randomly generate the rest.
+
+    1. Randomly select number of paths an AS will have, based on
+    num. of AS's - (1 + num. of already determined paths) = num. of paths
+
+    2. for each path, randomly select an AS to connect to and set the path,
+    as well as the path for the other AS
+
+    3. ???
+
+    4. profit $$$
+    """
+    # check if there are any user set paths we must respect
+    if any(network_data["routers"]["paths"].values()):
+        for router, paths in network_data["routers"]["paths"].items():
+            this_router_as = [
+                i
+                for i in network_data["AS"]["AS_contained_routers"]
+                if router in network_data["AS"]["AS_contained_routers"][i]
+            ][0]
+
+            for j in paths:
+                path_router_as = [
+                    i
+                    for i in network_data["AS"]["AS_contained_routers"]
+                    if j in network_data["AS"]["AS_contained_routers"][i]
+                ][0]
+
+                network_data["AS"]["paths"][this_router_as].add(path_router_as)
+                network_data["AS"]["paths"][path_router_as].add(this_router_as)
+
+    for i in range(as_num):
+        # generate a list of possible choices for paths
+        tmp_list = list(range(as_num))
+        tmp_list.remove(i)
+
+        # pick a random number of paths for the AS in question
+        choice_range = as_num - (1 + len(network_data["AS"]["paths"][f"AS{i+1}"]))
+        if choice_range < 2:
+            continue
+        num_of_paths = random.randrange(1, choice_range)
+
+        # pick a random AS to path to
+        for j in range(num_of_paths):
+            rnd_path = random.choice(tmp_list)
+            network_data["AS"]["paths"][f"AS{i+1}"].add(f"AS{rnd_path+1}")
+            network_data["AS"]["paths"][f"AS{rnd_path+1}"].add(f"AS{i+1}")
+            tmp_list.remove(rnd_path)
+
+    pprint(network_data)
+
+
+def print_path_table(net_dict):
+    """
+    Prints the data table for the custom paths
+    """
+    for key, item in list(net_dict["routers"]["paths"].items()):
+        paths_str = f"{key}:\t{item}".expandtabs(8)
+        print(paths_str)
+
+    print()
+
+
+def setup_as_routers(as_num, router_num):
+    """
+    Distributes the routers to the different AS's and polls the user for any specific
+    routes they might wish for.
+    """
+    print(
+        "By default, each AS will be connected to other random AS's.Hence the network \n"
+        "topology will be different each simulation, unless configured otherwise.\n"
+    )
+
+    network_dict = {
+        "AS": {
+            "AS_contained_routers": defaultdict(list),
+            "paths": defaultdict(set),
+        },
+        "routers": {"paths": {}},
+    }
+
+    # distribute the routers to their corresponding AS, based on simple modulo arithmetics
+    for i in range(router_num):
+        as_location = i % as_num
+        network_dict["AS"]["AS_contained_routers"][f"AS{as_location+1}"].append(
+            f"R{i+1}"
+        )
+
+        # create empty path sets for each router
+        network_dict["routers"]["paths"][f"R{i+1}"] = []
+
+    custom_paths = input(
+        "Do you wish to set any specific routes for any of the routers that will be created? Y/N\n"
+    ).replace(" ", "")
+    if custom_paths.upper() == "N":
+        return network_dict
+
+    if custom_paths.upper() != "Y":
+        print("Wrong answer :O Aborting...")
+        return network_dict
+
+    loop = True
+    while loop:
+        # Get the router to customise its path
+        router_of_choice = (
+            input(
+                f"Which router's path would you like to customise?\n"
+                f"Possible choices: {list(network_dict['routers']['paths'])}\n"
+                "You can exit the customisation at any point by selecting Q.\n"
+            )
+            .upper()
+            .replace(" ", "")
+        )
+        # exit or drop incorrect input
+        if router_of_choice == "Q":
+            loop = False
+            continue
+
+        if router_of_choice not in network_dict["routers"]["paths"]:
+            print("Incorrect value chosen. Aborting customization...\n")
+            continue
+
+        choice_path = input(
+            f"To which routers would you like your router to have a path?\n"
+            f"Possible choices: {list(network_dict['routers']['paths'])}\n"
+            "You can also set a list of routes you wish to add, e.g. <r1 r2 r4>.\n"
+            "You can exit the customisation at any point by selecting Q.\n"
+        ).upper()
+
+        paths = choice_path.split(" ")
+
+        if "Q" in paths:
+            loop = False
+            continue
+
+        # get the AS of the chosen router
+        router_as = [
+            i
+            for i in network_dict["AS"]["AS_contained_routers"]
+            if router_of_choice in network_dict["AS"]["AS_contained_routers"][i]
+        ]
+
+        # if any element in paths is also in the AS of the chosen router, we have some
+        # overlapping in paths that we do not want, skip the choice made
+        if bool(
+            set(paths) & set(network_dict["AS"]["AS_contained_routers"][router_as[0]])
+        ):
+            print("Incorrect value chosen. Aborting customization...\n")
+            continue
+
+        tmp_paths = set(network_dict["routers"]["paths"][router_of_choice])
+
+        if set(paths).issubset(tmp_paths):
+            print("Path(s) already contained. Aborting customization...\n")
+            continue
+
+        tmp_paths.update(set(paths))
+        network_dict["routers"]["paths"][router_of_choice] = list(tmp_paths)
+
+        print_path_table(network_dict)
+
+    return network_dict
 
 
 def start_simulation():
-    pass
+    """
+    Handles the simulation process and the creation of necessary objects.
+    """
